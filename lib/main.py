a683cb0152a90f3822cdd86253880163448aac50
Jessica Dunstan <jessica.lynn.dunstan@gmail.com>
2021-03-01 22:55:30 -0800

    Made URL requests python version agnostic. Next steps: Figure out best way to remove Beautiful Soup dependency.

--- lib/main.py
+++ lib/main.py
@@ -1,9 +1,30 @@
 
+# -*- coding: utf-8 -*-
+# ^ For Python2 backwards compatibility
 import os
+
+# Q: Why not use Requests library for handling this HTTPS stuff? Seems like a no-brainer!
+# A: Indeed! However, we're intentionally trying to keep this script free from external libraries,
+#    even when they would make the code easier to read.
+#
+#    The reason for this is that fewer dependencies means an easier install on all cast member's setups.
+#    (windows, mac, linux). Fewer system assumptions, etc. Just point a python version at it and go.
+
 # For making network requests.
-import requests
+#import requests
+import getpass
+import json
+import ssl
+
+try:
+    # Python 3
+    from http.client import HTTPSConnection
+except ImportError:
+    # Python 2
+    from httplib import HTTPSConnection
 
 from contact_scraper import parse_vcard_from_html
+
 
 def fetch_contact_page(password):
     """ Accesses
@@ -12,17 +33,22 @@ def fetch_contact_page(password):
         password {[string]} -- Password to enter Fortress. Be careful: NEVER hardcode or log this value.
     """
 
-    contact_info_url = "http://www.jetcityimprov.org/fortress/phone-numbers-and-emails/"
-    postpass_url = "http://www.jetcityimprov.org/wp-login.php?action=postpass"
-    body = { "post_password": password,
-             "Submit": "Enter" }
+    contact_info_url = "www.jetcityimprov.org/fortress/phone-numbers-and-emails/"
+    postpass_host = "www.jetcityimprov.org/wp-login.php?action=postpass"
+    host = "www.jetcityimprov.org"
+    body = { "body": { "post_password": getpass.getpass(), "Submit": "Enter" }}
     response_html = ""
-    with requests.session() as session:
-        # First, login with appropriate credentials to access the fortress page.
-        session.post(postpass_url, data=body)
-        # Now that we're logged in, fetch the contact page.
-        response = session.get(contact_info_url)
-        response_html = response.text
+
+    ssl_context = ssl.create_default_context() # To make sure we're connecting securely
+    
+    body_bytes = bytes(json.dumps(body), encoding='utf8')
+
+    https = HTTPSConnection(host, context=ssl_context)
+    https.connect()
+    https.request("POST", "/wp-login.php?action=postpass", body=body_bytes)
+    resp = https.getresponse()
+    result = resp.read()
+    response_html = bytes.decode(result, "utf-8")
     return response_html
 
 def save_contacts(vcard, output_path, output_filename):
